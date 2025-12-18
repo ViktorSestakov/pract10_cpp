80c323895598f3941059281ad0e040917bf1ce72
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2025-12-18 14:30:05 +0500

    переделано меню, доделать загрузчик

--- pract10_cpp/pract10_cpp.cpp
+++ pract10_cpp/pract10_cpp.cpp
@@ -43,77 +43,162 @@ DWORD ids[4];
 void Seeker(HANDLE thread, DWORD id) {
 	SuspendThread(thread);
 	if (id == ids[0]) {
-		cout << iter[0] << endl;
+		cout << iter[0];
 		iter[0] = 0;
 	}
 	else if (id == ids[1]) {
-		cout << iter[1] << endl;
+		cout << iter[1];
 		iter[1] = 0;
 	}
 	else if (id == ids[2]) {
-		cout << iter[2] << endl;
+		cout << iter[2];
 		iter[2] = 0;
 	}
 	ResumeThread(thread);
 }
 
-void change_priority(HANDLE thread) {
+void nagruzchik() {
+	SetThreadPriorityBoost(threads[3], FALSE);
+	SetThreadPriority(threads[3], THREAD_PRIORITY_HIGHEST);
+	Sleep(3000);
+	SetThreadPriority(threads[0], THREAD_PRIORITY_IDLE);
+	SetThreadPriority(threads[1], THREAD_PRIORITY_IDLE);
+	SetThreadPriority(threads[2], THREAD_PRIORITY_IDLE);
+	Sleep(3000);
+	SetThreadPriority(threads[3], THREAD_PRIORITY_NORMAL);
+
+	SetThreadPriorityBoost(threads[3], TRUE);
+}
+
+void menu_vibor() {
 	int vibor;
+	bool konec = false;
 
-	cout << "Выберите приоритет: " << endl;
-	cout << "1. Низший" << endl;
-	cout << "2. Ниже нормального" << endl;
-	cout << "3. Нормальный" << endl;
-	cout << "4. Выше нормального" << endl;
-	cout << "5. Высший" << endl;
-	cout << "Выберите пункт: ";
+	while (!konec) {
+		cin >> vibor;
 
-	cin >> vibor;
-
-	switch (vibor)
-	{
-	case 1:
-		if (!SetThreadPriority(thread, THREAD_PRIORITY_LOWEST)) {
-			cout << "Failed to set priority to Thread" << endl;
-			cout << GetLastError() << endl;
+		switch (vibor)
+		{
+		case 1:
+			if (!SetThreadPriority(threads[0], THREAD_PRIORITY_LOWEST)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 2:
+			if (!SetThreadPriority(threads[1], THREAD_PRIORITY_LOWEST)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 3:
+			if (!SetThreadPriority(threads[2], THREAD_PRIORITY_LOWEST)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 4:
+			if (!SetThreadPriority(threads[0], THREAD_PRIORITY_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 5:
+			if (!SetThreadPriority(threads[1], THREAD_PRIORITY_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 6:
+			if (!SetThreadPriority(threads[2], THREAD_PRIORITY_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 7:
+			if (!SetThreadPriority(threads[0], THREAD_PRIORITY_ABOVE_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 8:
+			if (!SetThreadPriority(threads[1], THREAD_PRIORITY_ABOVE_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 9:
+			if (!SetThreadPriority(threads[2], THREAD_PRIORITY_ABOVE_NORMAL)) {
+				cout << "Failed to set priority to Thread" << endl;
+				cout << GetLastError() << endl;
+			}
+			break;
+		case 0:
+			threads[3] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)nagruzchik, NULL, 0, &ids[3]);
+			break;
+		case 10:
+			if (!TerminateThread(threads[0], 0)) cout << GetLastError() << endl;
+			if (!TerminateThread(threads[1], 0)) cout << GetLastError() << endl;
+			if (!TerminateThread(threads[2], 0)) cout << GetLastError() << endl;
+			if (!TerminateThread(threads[3], 0)) cout << GetLastError() << endl;
+			system("cls");
+			konec = true;
+			break;
+		default:
+			cout << "Не правильно выбран пункт!" << endl;
+			break;
 		}
-		system("cls");
-		break;
-	case 2:
-		if (!SetThreadPriority(thread, THREAD_PRIORITY_BELOW_NORMAL)) {
-			cout << "Failed to set priority to Thread" << endl;
-			cout << GetLastError() << endl;
-		}
-		system("cls");
-		break;
-	case 3:
-		if (!SetThreadPriority(thread, THREAD_PRIORITY_NORMAL)) {
-			cout << "Failed to set priority to Thread" << endl;
-			cout << GetLastError() << endl;
-		}
-		system("cls");
-		break;
-	case 4:
-		if (!SetThreadPriority(thread, THREAD_PRIORITY_ABOVE_NORMAL)) {
-			cout << "Failed to set priority to Thread" << endl;
-			cout << GetLastError() << endl;
-		}
-		system("cls");
-		break;
-	case 5:
-		if (!SetThreadPriority(thread, THREAD_PRIORITY_HIGHEST)) {
-			cout << "Failed to set priority to Thread" << endl;
-			cout << GetLastError() << endl;
-		}
-		system("cls");
-		break;
-	default:
-		cout << "Не правильно выбран пункт!" << endl;
-		break;
 	}
+}
 
-	cout << "Приоритет изменен!" << endl;
-	cout << endl;
+void menu() {
+	HANDLE thread;
+
+	thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)menu_vibor, NULL, 0, NULL);
+
+	while (true) {
+		Sleep(1000);
+		system("cls");
+		cout << "Кол-во итераций в секундну" << endl;
+		cout << "Поток 1 - инкремент: ";
+		Seeker(threads[0], ids[0]);
+		cout << " | Приоритет = " << GetThreadPriority(threads[0]) << endl;
+		iter[0] = 0;
+		cout << "Поток 2 - последовательность фибоначчи: ";
+		Seeker(threads[1], ids[1]);
+		cout << " | Приоритет = " << GetThreadPriority(threads[1]) << endl;
+		iter[1] = 0;
+		cout << "Поток 3 - последовательность квадратов: ";
+		Seeker(threads[2], ids[2]);
+		cout << " | Приоритет = " << GetThreadPriority(threads[2]) << endl;
+		iter[2] = 0;
+		cout << endl;
+		if (GetThreadPriority(threads[3]) == 2 || GetThreadPriority(threads[3]) == 0) {
+			cout << "Нагрузчик | Приоритет = " << GetThreadPriority(threads[3]) << endl;
+		}
+		else {
+			cout << "Нагрузчик | Приоритет = неактивен" << endl;
+		}
+		cout << endl;
+
+		cout << "Меню:" << endl;
+		cout << "1. Сменить приоритет потока 1 - минимальный" << endl;
+		cout << "2. Сменить приоритет потока 2 - минимальный" << endl;
+		cout << "3. Сменить приоритет потока 3 - минимальный" << endl;
+		cout << endl;
+		cout << "4. Сменить приоритет потока 1 - нормальный" << endl;
+		cout << "5. Сменить приоритет потока 2 - нормальный" << endl;
+		cout << "6. Сменить приоритет потока 3 - нормальный" << endl;
+		cout << endl;
+		cout << "7. Сменить приоритет потока 1 - выше нормального" << endl;
+		cout << "8. Сменить приоритет потока 2 - выше нормального" << endl;
+		cout << "9. Сменить приоритет потока 3 - выше нормального" << endl;
+		cout << endl;
+		cout << "0. Запустить нагрузчик" << endl;
+		cout << "10. Выйти из программы" << endl;
+		cout << endl;
+		cout << "Введите номер пункта: ";
+	}
 }
 
 int main()
@@ -123,7 +208,6 @@ int main()
 	threads[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Inkrement, NULL, 0, &ids[0]);
 	threads[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Fibonachi, NULL, 0, &ids[1]);
 	threads[2] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Kvadraty, NULL, 0, &ids[2]);
-	threads[3] = CreateThread(NULL, 0, NULL, NULL, 0, &ids[3]); // доделать нагрузчик
 
 	if (threads[0] == 0 || threads[1] == 0 || threads[2] == 0) {
 		return GetLastError();
@@ -140,70 +224,16 @@ int main()
 			cout << "Failed to set priority to Thread-3" << endl;
 			return GetLastError();
 		}
-		if (!SetThreadPriority(threads[3], THREAD_PRIORITY_LOWEST)) {
-			cout << "Failed to set priority to Thread-4" << endl;
-			return GetLastError();
-		}
 	}
 
-	int vibor;
-	bool konec = false;
+	HANDLE thread;
 
-	while (!konec) {
-		cout << "Кол-во итераций в секундну" << endl;
-		Sleep(1000);
-		cout << "Поток 1 - инкремент: ";
-		Seeker(threads[0], ids[0]);
-		iter[0] = 0;
-		cout << "Поток 2 - последовательность фибоначчи: ";
-		Seeker(threads[1], ids[1]);
-		iter[1] = 0;
-		cout << "Поток 3 - последовательность квадратов: ";
-		Seeker(threads[2], ids[2]);
-		iter[2] = 0;
-		cout << endl;
-
-		cout << "Меню:" << endl;
-		cout << "1. Сменить приоритет потока 1" << endl;
-		cout << "2. Сменить приоритет потока 2" << endl;
-		cout << "3. Сменить приоритет потока 3" << endl;
-		cout << "4. Запустить нагрузчик" << endl;
-		cout << "5. Выйти из программы" << endl;
-		cout << endl;
-		cout << "Введите номер пункта: ";
-
-		cin >> vibor;
-
-		switch (vibor)
-		{
-		case 1:
-			change_priority(threads[0]);
-			break;
-		case 2:
-			change_priority(threads[1]);
-			break;
-		case 3:
-			change_priority(threads[2]);
-			break;
-		case 4:
-			// вызов функции нагрузчика
-			break;
-		case 5:
-			CloseHandle(threads[0]);
-			CloseHandle(threads[1]);
-			CloseHandle(threads[2]);
-			system("cls");
-			konec = true;
-			break;
-		default:
-			cout << "Не правильно выбран пункт!" << endl;
-			break;
-		}
-	}
+	thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)menu, NULL, 0, NULL);
 
 	WaitForSingleObject(threads[0], INFINITE);
 	WaitForSingleObject(threads[1], INFINITE);
 	WaitForSingleObject(threads[2], INFINITE);
+	WaitForSingleObject(threads[3], INFINITE);
 
 	return 0;
 }
